ae81e0aa1ab4aff52188ec4e05cd40a75b1b34a6
Harrys <harrys@Harryss-MacBook-Pro.local>
2019-12-13 19:31:00 +0530

    testing

--- app/src/main/java/com/macom/medicationapp/SecondPage.java
+++ app/src/main/java/com/macom/medicationapp/SecondPage.java
@@ -1,8 +1,13 @@
 package com.macom.medicationapp;
 
 import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,6 +15,7 @@ import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 
 import ca.antonious.materialdaypicker.MaterialDayPicker;
@@ -64,19 +70,48 @@ if(timeMinute<10){
            }
             stringBuffer.append(materialDayPicker.getSelectedDays().get(days-1));
 
-            databaseHelper.addReminder(new MedicinReminderModel(title, stringBuffer.toString(),time));
+                scheduleNotification(getNotification("test"),3000);
+
+
+                databaseHelper.addReminder(new MedicinReminderModel(title, stringBuffer.toString(),time));
             Intent i = new Intent(SecondPage.this, MainActivity.class);
             startActivity(i);
-
 
         }
         else {
 
 
             }
+
+
         }
 
         );
+        }
+
+
+        private void scheduleNotification(Notification notification, int timeDelay){
+Intent notificationIntent = new Intent(getApplicationContext(),NotificationPublisher.class);
+
+notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID,1);
+notificationIntent.putExtra(NotificationPublisher.NOTIFICATION,notification);
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(this,0,notificationIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+long futureTimeInMillis = timeDelay;
+            AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+            alarmManager.set(AlarmManager.RTC_WAKEUP,futureTimeInMillis,pendingIntent);
+
+        }
+
+        private Notification getNotification(String title){
+
+NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+builder.setContentTitle("Test Alarm");
+builder.setContentText("Test-01");
+builder.setSmallIcon(R.drawable.circle_toggle_button);
+return builder.build();
+
         }
 
     }
